7e25a2f5145d798b76366bcba92875db98081933
mahesh <madu140@gmail.com>
2021-09-27 12:56:44 +0530

    User details Update feature and User delete feature for MOH ADMIN v0.0.4

--- src/main/java/com/novax/covidtrackerbackend/controller/UserControllers/MOHAdminController.java
+++ src/main/java/com/novax/covidtrackerbackend/controller/UserControllers/MOHAdminController.java
@@ -1,24 +1,29 @@
 package com.novax.covidtrackerbackend.controller.UserControllers;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.novax.covidtrackerbackend.model.User;
 import com.novax.covidtrackerbackend.response.Response;
 import com.novax.covidtrackerbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "management/api/V1/MOH/admin")
-@PreAuthorize("hasAnyRole('ROLE_MOH_ADMIN,ROLE_HOSPITAL_ADMIN')")
+@PreAuthorize("hasAnyRole('ROLE_MOH_ADMIN')")
 public class MOHAdminController {
 
     private final UserService userService;
@@ -35,9 +40,6 @@ public class MOHAdminController {
      * DASHBOARD RELATED DATA
      * @return - returns copy of persisted user information as JSON object.
      */
-
-
-
 
     @GetMapping("/dashboard")
     @PreAuthorize("hasAuthority('moh_admin:read')")
@@ -65,18 +67,56 @@ public class MOHAdminController {
 
     /**
      * DELETES MOH_USER/HOSPITAL_ADMIN
-     * @param nic - NIC of the targeted user to delete.
+     * @param u_id - NIC of the targeted user to delete.
      * @return - If database validation passes, returns copy of deleted user information as JSON object.
      */
+    @DeleteMapping("/delete/{u_id}")
+    @PreAuthorize("hasAuthority('hospital_admin:write')")
+    public ResponseEntity<HashMap<String, Object>> deleteUser(@PathVariable Long u_id,HttpServletRequest request) throws SQLException {
+        Response response = new Response();
+        Optional<User> user = userService.getUserById(u_id);
+        if(user.isPresent()){
+            User u = user.get();
+            if(u.getRole().equals("MOH_ADMIN") || u.getRole().equals("MOH_USER")){
+                // if no exception occurred send this response
+                response.setResponseCode(HttpStatus.OK.value())
+                        .setMessage("request success")
+                        .setURI(request.getRequestURI())
+                        .addField("Deleted",u);
+                userService.deleteUser(u_id);
+            }else{
+                throw new EmptyResultDataAccessException("You don't have permission to delete this type of users",0);
+            }
+        }
+        return response.getResponseEntity();
+    }
 
-    @DeleteMapping("/user/delete")
-    @PreAuthorize("hasAuthority('moh_admin:write')")
-    public String deleteMOHUser(@RequestBody String nic){
+    /**
+     *
+     * @param newDetailsOfUser - updated details sent by the client
+     * @param request - HttpServletRequest object to access uri
+     * @param auth - Authentication object in the application context to access authenticated user's user_id
+     * @return - updated user details with response
+     * @throws SQLException
+     */
+    @PostMapping("/update/details")
+    @PreAuthorize("hasAnyRole('ROLE_MOH_ADMIN')")
+    @JsonView(User.WithoutPasswordView.class)
+    public ResponseEntity<HashMap<String, Object>> updateMOHUserDetails(@Valid @RequestBody User newDetailsOfUser, HttpServletRequest request, Authentication auth) throws SQLException {
+        Response response = new Response();
+        User updatedDetailsOfUser = userService.updateUserDetails(newDetailsOfUser,auth);
+        // send this response if data update is success
+        // exclude unwanted details (pw)
+        MappingJacksonValue value = new MappingJacksonValue(updatedDetailsOfUser);
+        value.setSerializationView(User.WithoutPasswordView.class);
+        User useWithOutPasswordView = (User)  value.getValue();
 
-        // deleting a user
-        // business logic
-
-        return "user deleted";
+        response.reset()
+                .setMessage("details updated")
+                .setURI(request.getRequestURI())
+                .setResponseCode(HttpServletResponse.SC_OK)
+                .addField("updatedInfo",useWithOutPasswordView);
+        return response.getResponseEntity();
     }
 
     /**
