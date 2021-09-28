a1e7a6e1483399d296af5d7519f0f4f44609c8ea
roshinie <roshinie.18@cse.mrt.ac.lk>
2021-09-28 13:43:58 +0530

    Add new method to get user

--- src/main/java/com/novax/covidtrackerbackend/controller/UserControllers/HospitalAdminController.java
+++ src/main/java/com/novax/covidtrackerbackend/controller/UserControllers/HospitalAdminController.java
@@ -1,18 +1,24 @@
 package com.novax.covidtrackerbackend.controller.UserControllers;
 
 import com.novax.covidtrackerbackend.model.User;
+import com.novax.covidtrackerbackend.model.dao.UserDAO;
 import com.novax.covidtrackerbackend.response.Response;
+import com.novax.covidtrackerbackend.service.UserDAOService;
 import com.novax.covidtrackerbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
+import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.io.IOException;
@@ -26,6 +32,8 @@ public class HospitalAdminController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserDAOService userDAOService;
     @Autowired
     private PasswordEncoder passwordEncoder;
 
@@ -42,7 +50,23 @@ public class HospitalAdminController {
                 .setURI(request.getRequestURI())
                 .addField("userInfo", new_user);
         return response.getResponseEntity();
+    }
 
+    @GetMapping("/user/{email}")
+    public ResponseEntity<HashMap<String, Object>> getUserByEmail( @PathVariable("email") String userEmail){
+        try{
+            UserDAO user = (UserDAO) userDAOService.loadUserByUsername(userEmail);
+            Response<Object> response = new Response<>();
+            response.setResponseCode(HttpStatus.OK.value())
+                    .addField("userInfo", user);
+            return response.getResponseEntity();
 
+        } catch (EntityNotFoundException e) {
+            System.out.println("Error");
+            Response<Object> response = new Response<>();
+            response.setResponseCode(HttpStatus.NOT_FOUND.value())
+                    .setException(e);
+            return response.getResponseEntity();
+        }
     }
 }
