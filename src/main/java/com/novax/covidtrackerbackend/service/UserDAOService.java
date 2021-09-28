eb43206a6449639057e403e19093dc718f03c047
roshinie <roshinie.18@cse.mrt.ac.lk>
2021-09-28 20:51:31 +0530

    Update user dao services

--- src/main/java/com/novax/covidtrackerbackend/service/UserDAOService.java
+++ src/main/java/com/novax/covidtrackerbackend/service/UserDAOService.java
@@ -4,9 +4,6 @@ import com.novax.covidtrackerbackend.model.dao.UserDAO;
 import com.novax.covidtrackerbackend.repository.UserDAORepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
@@ -20,8 +17,17 @@ public class UserDAOService {
     @Autowired
     private final UserDAORepository userDAORepository;
 
-    public UserDAO loadUserByUsername(String userEmail) throws UsernameNotFoundException {
-        Optional<UserDAO> result = userDAORepository.findByEmail(userEmail);
+    public UserDAO loadUserByNicHospitalId(String userNic, Integer hospitalId)  {
+        Optional<UserDAO> result = userDAORepository.findByNicAndHospitalusersHospital(userNic , hospitalId);
+        if (result.isPresent()) {
+            return result.get();
+        } else {
+            throw new EntityNotFoundException("User not found");
+        }
+    }
+
+    public UserDAO loadUserByEmailNHospitalId(String userEmail, Integer hospitalId) {
+        Optional<UserDAO> result = userDAORepository.findByEmailAndHospitalusersHospital(userEmail, hospitalId);
         if (result.isPresent()) {
             return result.get();
         } else {
