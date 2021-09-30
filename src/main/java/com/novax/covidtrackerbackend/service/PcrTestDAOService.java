e3d2d8a50a50fa37999f1163b3f2c7d9c774eb6a
roshinie <roshinie.18@cse.mrt.ac.lk>
2021-10-01 01:31:59 +0530

    Add update method for pcr test status

--- src/main/java/com/novax/covidtrackerbackend/service/PcrTestDAOService.java
+++ src/main/java/com/novax/covidtrackerbackend/service/PcrTestDAOService.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.sql.SQLException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -32,5 +34,25 @@ public class PcrTestDAOService {
                 testData.getTest_result());
         pcrTestDAORepository.save(pcr);
         return pcr;
+    }
+
+    /**
+     * UPDATE PCR TEST STATE
+     * @param test_id Unique ID of the test which needs to be updated
+     * @param test_state New value for the test status
+     * @return updates pcr test details
+     */
+
+    public PcrTestDAO updatePcrTestStatus( Integer test_id,  String test_state)  throws SQLException {
+        Optional<PcrTestDAO> testFound = pcrTestDAORepository.findById(test_id);
+        if (testFound.isEmpty()){
+            throw new EntityNotFoundException("Invalid Test ID Provided");
+        }
+        PcrTestDAO testData = testFound.get();
+        if (test_state != null && !testData.getTest_result().equals(test_state)){
+            testData.setTest_result(test_state);
+        }
+        pcrTestDAORepository.save(testData);
+        return testData;
     }
 }
