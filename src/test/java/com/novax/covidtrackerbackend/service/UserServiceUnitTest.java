ca2223021197e6a3c747b78210f693558378016f
mahesh <madu140@gmail.com>
2021-10-16 19:25:55 +0530

    unit tests added - UserDAOServiceUnitTest - PcrTestDAOServiceUnitTest - HospitalServiceUnitTest - UserServiceUnitTest

--- src/test/java/com/novax/covidtrackerbackend/service/UserServiceUnitTest.java
+++ src/test/java/com/novax/covidtrackerbackend/service/UserServiceUnitTest.java
@@ -9,7 +9,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.sql.SQLException;
 import java.util.Collection;
