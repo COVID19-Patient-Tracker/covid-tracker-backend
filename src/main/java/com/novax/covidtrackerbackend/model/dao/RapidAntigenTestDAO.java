c43192d2fa7d57a2edd60559f9f0fc73c9e2901a
roshinie <roshinie.18@cse.mrt.ac.lk>
2021-10-18 17:09:16 +0530

    Add antigen test related functions

---
+++ src/main/java/com/novax/covidtrackerbackend/model/dao/RapidAntigenTestDAO.java
@@ -0,0 +1,49 @@
+package com.novax.covidtrackerbackend.model.dao;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "rapidantigentest")
+public class RapidAntigenTestDAO {
+
+    @Getter
+    @Setter
+    @Column(name = "antigen_test_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Getter
+    @Setter
+    private Long patient_id;
+
+    @Getter
+    @Setter
+    private int hospital_id;
+
+    @Getter
+    @Setter
+    private Date test_data;
+
+    @Getter
+    @Setter
+    private String test_result;
+
+    public RapidAntigenTestDAO(Long patient_id, int hospital_id, Date test_data, String test_result) {
+        this.patient_id = patient_id;
+        this.hospital_id = hospital_id;
+        this.test_data = test_data;
+        this.test_result = test_result;
+    }
+}
---
+++ src/main/java/com/novax/covidtrackerbackend/service/RapidAntigenTestDAOService.java
@@ -0,0 +1,56 @@
+package com.novax.covidtrackerbackend.service;
+
+import com.novax.covidtrackerbackend.model.dao.RapidAntigenTestDAO;
+import com.novax.covidtrackerbackend.model.dto.AddTestRequestDTO;
+import com.novax.covidtrackerbackend.repository.RapidAntigenTestDAORepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.sql.SQLException;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RapidAntigenTestDAOService {
+
+    @Autowired
+    private final RapidAntigenTestDAORepository rapidAntigenTestDAORepository;
+
+    /**
+     * ADD NEW RAPIDANTIGEN TEST RECORD TO `rapidantigentest` TABLE
+     * @param testData
+     * @return created Antigen Test Record data
+     */
+
+    public RapidAntigenTestDAO addAntigenTest(AddTestRequestDTO testData)  throws SQLException {
+        RapidAntigenTestDAO antigenTest = new RapidAntigenTestDAO(
+                testData.getPatient_id(),
+                testData.getHospital_id(),
+                testData.getTest_data(),
+                testData.getTest_result());
+        rapidAntigenTestDAORepository.save(antigenTest);
+        return antigenTest;
+    }
+
+    /**
+     * UPDATE ANTIGEN TEST STATE
+     * @param test_id Unique ID of the test which needs to be updated
+     * @param test_state New value for the test status
+     * @return updates antigen test details
+     */
+
+    public RapidAntigenTestDAO updateAntigenTestStatus( Integer test_id,  String test_state)  throws SQLException {
+        Optional<RapidAntigenTestDAO> testFound = rapidAntigenTestDAORepository.findById(test_id);
+        if (testFound.isEmpty()){
+            throw new EntityNotFoundException("Invalid Test ID Provided");
+        }
+        RapidAntigenTestDAO testData = testFound.get();
+        if (test_state != null && !testData.getTest_result().equals(test_state)){
+            testData.setTest_result(test_state);
+        }
+        rapidAntigenTestDAORepository.save(testData);
+        return testData;
+    }
+}
