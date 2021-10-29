3eb6ab2da49edf381cf71676f133792f19edd3af
Roshinie Jayasundara <roshinie.18@cse.mrt.ac.lk>
2021-10-29 15:38:31 +0530

    Add new dto for patient signup

---
+++ src/main/java/com/novax/covidtrackerbackend/model/dto/PatientRegisterDTO.java
@@ -0,0 +1,34 @@
+package com.novax.covidtrackerbackend.model.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+public class PatientRegisterDTO {
+    @Getter
+    @Setter
+    @NotNull(message = "patient id cannot be null")
+    private Long patient_id;
+
+    @Getter
+    @Setter
+    @NotNull(message = "nic cannot be null")
+    private String nic;
+
+    @Getter
+    @Setter
+    @NotEmpty(message = "email cannot be null")
+    @Email
+    private String email;
+
+    @Getter
+    @Setter
+    @Size(min = 8,max = 100,message = "size does not match")
+    private String password;
+}
