1ae8649e7a7fc87c8794fb37c73870b303983043
mahesh <madu140@gmail.com>
2021-11-05 12:13:53 +0530

    update covid patient table when updating or adding RapidAntigen or PCR

--- src/main/java/com/novax/covidtrackerbackend/service/PcrTestDAOService.java
+++ src/main/java/com/novax/covidtrackerbackend/service/PcrTestDAOService.java
@@ -1,7 +1,11 @@
 package com.novax.covidtrackerbackend.service;
 
+import com.novax.covidtrackerbackend.model.CovidPatient;
+import com.novax.covidtrackerbackend.model.Hospital;
 import com.novax.covidtrackerbackend.model.dao.PcrTestDAO;
 import com.novax.covidtrackerbackend.model.dto.AddTestRequestDTO;
+import com.novax.covidtrackerbackend.repository.CovidPatientRepository;
+import com.novax.covidtrackerbackend.repository.HospitalRepository;
 import com.novax.covidtrackerbackend.repository.PcrTestDAORepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +13,16 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class PcrTestDAOService {
-
+    @Autowired
+    private final CovidPatientRepository covidPatientRepository;
+    @Autowired
+    private final HospitalRepository hospitalRepository;
     @Autowired
     private final PcrTestDAORepository pcrTestDAORepository;
 
@@ -31,6 +38,19 @@ public class PcrTestDAOService {
                 testData.getHospital_id(),
                 testData.getTest_data(),
                 testData.getTest_result());
+
+
+        // when updating PCR results if it is positive create new covid patient in table
+        if(pcr.getTest_result().equals("POSITIVE")){
+            Hospital hospital = hospitalRepository.findById(pcr.getHospital_id()).get();
+            CovidPatient covidPatient = new CovidPatient(
+                    pcr.getPatientId(),
+                    hospital,
+                    new Date(),
+                    "ACTIVE"
+            );
+            covidPatientRepository.save(covidPatient);
+        }
         pcrTestDAORepository.save(pcr);
         return pcr;
     }
@@ -50,6 +70,21 @@ public class PcrTestDAOService {
         PcrTestDAO testData = testFound.get();
         if (test_state != null && !testData.getTest_result().equals(test_state)){
             testData.setTest_result(test_state);
+
+            // when updating PCR results if it is positive create new covid patient in table
+            if(testData.getTest_result().equals("POSITIVE")){
+
+                Hospital hospital = hospitalRepository.findById(testData.getHospital_id()).get();
+
+                CovidPatient covidPatient = new CovidPatient(
+                        testData.getPatientId(),
+                        hospital,
+                        new Date(),
+                        "ACTIVE"
+                );
+
+                covidPatientRepository.save(covidPatient);
+            }
         }
         pcrTestDAORepository.save(testData);
         return testData;
